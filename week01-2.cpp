230821ec7cced62bee0adf422e1baaf6dbe38c72
Cappuccnio <91250379+kapumochi@users.noreply.github.com>
2024-09-12 16:20:24 +0800

    week01

---
+++ week01-2.cpp
@@ -0,0 +1,10 @@
+//week01-2.cpp
+#include <iostream>
+using namespace std; //使用命名空間 std
+int main()
+{
+	int n;
+	cin >> n;
+
+	cout << n;
+}
---
+++ week01-4a.cpp
@@ -0,0 +1,14 @@
+//week01-4a
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	cout << n/2 %2;
+	cout << n/2 %2;
+	cout << n%2;
+	return 0;
+}
