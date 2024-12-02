20bfbcc5da4169daa0c32d48d4aa68014ee57a23
Cappuccnio <91250379+kapumochi@users.noreply.github.com>
2024-12-02 18:57:59 +0800

    Update week12-6.cpp

--- week12/week12-6.cpp
+++ week12/week12-6.cpp
@@ -9,10 +9,11 @@ int main()
 	cin >> T;
 	for(int t=1;t<=T; t++){
 		char c, c2;
-		cin >> c >> c2 >> N;
-		for(int i=0; i<N*N; i++) cin >> a[i];
-		int bad=0;
-		for(int i=0; i<N*N; i++){
+		cin >> c >> c2 >> N; //每次有新矩陣時,input都會放一個 N = ?,所以在迴圈裏面
+		for(int i=0; i<N*N; i++) cin >> a[i];//輸入矩陣
+		int bad=0; //決定是不是對稱矩陣的變數
+		
+		for(int i=0; i<N*N; i++){ //判斷式
 			if(a[i]<0) bad=1;
 			if(a[i] != a[N*N-1-i]) bad=1;
 		}
